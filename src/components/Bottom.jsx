8a05a6685fb80ac6e100665d7f8b767bf10e1fe0
choiseungryul <sr3057@naver.com>
2025-08-01 12:34:22 +0900

    component 최종 수정

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -1,28 +1,38 @@
-function Bottom () {
+import {Link} from 'react-router-dom';
+import ScrollTop from './ScrollTop';
+
+function Bottom() {
   return (
-    <div className='bottom'>
-      <a href="http://localhost:5173/" style={{color:'lightgray'}}>
-        <h3>여행합니다</h3>
-      </a>
-      <ul>
-        <li>
-          <a href="http://localhost:5173/" style={{color:'lightgray'}}>Home</a>
-        </li>
-        <li>
-          <a href="http://localhost:5173/introduce" style={{color:'lightgray'}}>Introduce</a>
-        </li>
-        <li>
-          <a href="http://localhost:5173/contents" style={{color:'lightgray'}}>business</a>
-        </li>
-         <a href="https://www.youtube.com/@YEOMI" target='_blank' rel="noopener noreferrer" className='image-link'>
-          <i class="fa-brands fa-youtube fa-2x " style={{color: 'red'}}></i>
-        </a>
-        <a href="https://www.instagram.com/yeomi.travel/" target='_blank' rel="noopener noreferrer" className='image-link'>
-          <i class="fa-brands fa-square-instagram fa-2x" style={{color: 'pink'}}></i>
-        </a>
-      </ul>
+    <div className="bottom">
+      <div className="logo">
+        <ScrollTop />
+        <Link to="/">
+          <h3>여행합니다</h3>
+        </Link>
+      </div>
+      <div className="right-content">
+        <ul className="text-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/introduce">Introduce</Link>
+          </li>
+          <li>
+            <Link to="/contents">Contents</Link>
+          </li>
+        </ul>
+        <div className="icon-links">
+          <a href="https://www.youtube.com/@YEOMI" target="_blank" rel="noopener noreferrer" className="image-link">
+            <i className="fa-brands fa-youtube fa-2x" style={{ color: 'red' }}></i>
+          </a>
+          <a href="https://www.instagram.com/yeomi.travel/" target="_blank" rel="noopener noreferrer" className="image-link">
+            <i className="fa-brands fa-square-instagram fa-2x" style={{ color: 'pink' }}></i>
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Bottom;
